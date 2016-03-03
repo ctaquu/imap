dd1bcf24d5d47e8555e0d15eb6a95a8f84e57e5c
Damjan Veljkovic <damjan.veljkovic@knip.ch>
2016-03-03 14:45:41 +0100

    test

--- src/Parameters.php
+++ src/Parameters.php
@@ -33,6 +33,7 @@ class Parameters
     
     protected function decode($value)
     {
+        // this is it... :)
         $decoded = '';
         $parts = imap_mime_header_decode($value);
         foreach ($parts as $part) {
